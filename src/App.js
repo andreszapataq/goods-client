4058648cf2848b8c06a7d1316f4c6c3071b6c14d
andreszapataq <andreszapataq@gmail.com>
2022-08-01 22:39:07 -0500

    App fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { ITEMS } from "./data/data";
+import { useState, useEffect } from "react";
 import FilterableItemTable from "./components/FilterableItemTable";
 
 function App() {
+  const [inventario, setInventario] = useState([])
+
+  useEffect(() => {
+    const getInventario = async () => {
+      await fetchInventario()
+    }
+
+    getInventario()
+  })
+
+  const fetchInventario = async () => {
+    const res = await fetch('http://localhost:5008/api/v1/inventario')
+
+    const data = await res.json()
+
+    setInventario(data.data)
+  }
+
   return (
     <div className="App">
-      <FilterableItemTable items={ITEMS} />
+      <FilterableItemTable items={inventario} />
     </div>
   );
 }
